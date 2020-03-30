bba839d5276a4da599104e863f695a5509eb4bbd
Cyborg2017 <wangyang0987654@gmail.com>
2020-03-30 12:55:42 +0800

    nx529j: rootdir: Set permissions for nubia light

--- rootdir/etc/init.qcom.sh
+++ rootdir/etc/init.qcom.sh
@@ -354,7 +354,7 @@ case "$target" in
 				    fi
 				    ;;
 				"MTP" | "QRD")
-				# commented for nubia nx569j has keys
+				# commented for nubia msm8952/76 devices has keys
 #				       setprop qemu.hw.mainkeys 0
 				       ;;
 		      esac
